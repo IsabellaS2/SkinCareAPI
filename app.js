786b3293839fc230bcd2379b6a60b145f1f5a10e
IsabellaSulisufi <114592322+IsabellaSulisufi@users.noreply.github.com>
2024-12-05 12:26:23 +0000

    Create all three databases and add some products

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express ();
-const db = require("./database.js")
-
-
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db.sqlite');
 
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
@@ -12,27 +11,40 @@ app.listen(PORT, () => {
  });
 
 
- app.get("/status", (request, response) => {
-   const status = {
-      'Status': 'Running'
-   };
-   
-   response.send(status);
+ app.get('/status', (req, res) => {
+  res.json({ status: 'Running' });
 });
 
+const SQLCommandGetAll = `
+SELECT 
+    p.id AS product_id,
+    p.product_name,
+    p.product_type,
+    p.brand,
+    COALESCE(GROUP_CONCAT(DISTINCT rst.skin_type), '') AS recommended_skin_types,
+    COALESCE(GROUP_CONCAT(DISTINCT i.ingredient), '') AS ingredient_list
+FROM 
+    Products p
+LEFT JOIN 
+    RecommendedSkinTypes rst ON p.id = rst.product_id
+LEFT JOIN 
+    Ingredients i ON p.id = i.product_id
+GROUP BY 
+    p.id;
+`;
 
+app.get("/products", (req, res) => {
+  var sql = SQLCommandGetAll;  
+  var params = [];
 
-app.get("/products", (req, res, next) => {
-   var sql = "select * from user"
-   var params = []
-   db.all(sql, params, (err, rows) => {
-       if (err) {
-         res.status(400).json({"error":err.message});
-         return;
-       }
-       res.json({
-           "message":"success",
-           "data":rows
-       })
-     });
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({"error": err.message});
+      return;
+    }
+    res.json({
+      "message": "success",
+      "data": rows 
+    });
+  });
 });
